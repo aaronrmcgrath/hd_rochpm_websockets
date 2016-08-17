9772c59970268935f716e9556f643c300f6dd3ba
Mbob023 <armcgrathmedia@gmail.com>
2016-08-16 23:29:51 -0500

    added time

--- public/client.js
+++ public/client.js
@@ -6,26 +6,32 @@
 
 
 $(function() {
-  
+
   // keep a queue in case we get overloaded with tweets from a popular twitter search
   var queue = [];
   var events = [];
-  
+
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  console.log(date);
+
   // Listen for new tweets pushed from the server
   /*
   var socket = io();
   socket.on('twitter', function(tweet){
     queue.push(tweet);
   });
-  
+
   // seed the first tweet
   $.get('/tweets', function(tweet) {
       displayTwitter(tweet.text);
   });
-  *
-  
+  */
+
   $.get('/city-lights', function(req, res) {
-      // events = data;
+      events = res;
       console.log('In Client, here is res: ', res);
   });
 });
